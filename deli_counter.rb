edce91e9b5b053791995508ed725b4e6e1dd2e33
David Tewkesbury <tewkesburyd@hotmail.com>
2022-12-14 09:42:07 -0700

    commit tree 04664b237d6343ffae52d7f4eb8b37102ba91c90

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+require 'pry'
+
+def line deli
+    if deli.empty? == true
+        puts "The line is currently empty."
+    else
+        currentLine = "The line is currently:"
+        deli.each.with_index(1) do |person, index|
+            currentLine << " #{index}. #{person}"
+        end
+        puts currentLine 
+    end
+end
+
+def take_a_number deli, name
+    if deli.empty? == true
+        deli << name
+        puts "Welcome, #{name}. You are number 1 in line."
+    else
+        deli << name
+        puts "Welcome, #{name}. You are number #{deli.length} in line."
+    end
+end
+
+def now_serving deli
+    if deli.empty? == true
+        puts "There is nobody waiting to be served!"
+    else
+        puts  "Currently serving #{deli[0]}."
+        deli.shift
+    end
+end
+
+
